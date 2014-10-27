f5eb8f271de52d782b94a4a6c2b59d1292d73dc0
lleraromero <leandro.lera.romero@gmail.com>
2014-10-26 23:23:20 -0300

    Generic Stack example

---
+++ Examples/Stack.cs
@@ -0,0 +1,49 @@
+﻿using System.Diagnostics.Contracts;
+
+namespace Examples
+{
+    public class Stack<T>
+    {
+        public int Max { get; set; }
+        public int Next { get; set; }
+
+        public Stack()
+        {
+            Contract.Ensures(Max == 5 && Next == -1);
+
+            Max = 5;
+            Next = -1;
+        }
+
+        public Stack(int size)
+        {
+            Contract.Requires(size > 2);
+            Contract.Ensures(Max == size && Next == -1);
+
+            Max = size;
+            Next = -1;
+        }
+
+        [ContractInvariantMethod]
+        private void Invariant()
+        {
+            Contract.Invariant(Max > 2 && Next >= -1 && Max > Next);
+        }
+
+        public void Pop()
+        {
+            Contract.Requires(Next > -1);
+            Contract.Ensures(Next == Contract.OldValue<int>(Next) - 1);
+
+            Next = Next - 1;
+        }
+
+        public void Push<T>(T elem)
+        {
+            Contract.Requires(Next < Max);
+            Contract.Ensures(Next == Contract.OldValue<int>(Next) + 1);
+
+            Next = Next + 1;
+        }
+    }
+}
