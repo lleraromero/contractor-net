399b7228e40430e4447c8f0fd6873548c7b44e81
lleraromero <leandro.lera.romero@gmail.com>
2014-11-05 17:25:45 -0300

    Fix query naming, now it is using the same method that BCT uses

--- Core/CorralAnalyzer.cs
+++ Core/CorralAnalyzer.cs
@@ -122,14 +122,9 @@ namespace Contractor.Core
             var queryType = queryAssembly.DecompiledModule.AllTypes.Find(x => x.Name == typeToAnalyze.Name) as NamespaceTypeDefinition;
             foreach (var query in queries)
             {
-                StringBuilder queryName = new StringBuilder();
-                // Method name
-                queryName.Append(string.Concat(queryType.ResolvedType.ToString(), ".", query.Name.Value));
-                // If it has parameters their types also appear in the name of the query in boogie
-                foreach (var p in query.Parameters)
-                    queryName.Append("$").Append(p.Type.ResolvedType.ToString());
+                var queryName = CreateUniqueMethodName(query);
 
-                string output = RunCorral(queryName.ToString());
+                string output = RunCorral(queryName);
                 const string pattern = @"(true bug)|(reached recursion bound)|(has no bugs)";
                 Regex outputParser = new Regex(pattern, RegexOptions.ExplicitCapture |
                                                 RegexOptions.Multiline | RegexOptions.Compiled | RegexOptions.IgnoreCase);
@@ -182,8 +177,7 @@ namespace Contractor.Core
 
             var args = string.Format("{0} /main:{1} /recursionBound:{2}", GetQueryAssemblyPath().Replace("tmp", "bpl"), method, 3);    // recursionBound 3 es absolutamente arbitrario :)
 
-            var timer = new Stopwatch();
-            timer.Start();
+            var timer = Stopwatch.StartNew();
 
             try
             {
@@ -194,8 +188,8 @@ namespace Contractor.Core
             }
             catch (Exception ex)
             {
-                LogManager.Log(LogLevel.Fatal, ex.Message);
-                throw ex;
+                LogManager.Log(LogLevel.Fatal, ex);
+                throw;
             }
             
             timer.Stop();
@@ -271,5 +265,61 @@ namespace Contractor.Core
 
             return analysisResult;
         }
+
+        #region BCT-TranslationHelper.cs
+        public static string CreateUniqueMethodName(IMethodReference method)
+        {
+            var containingTypeName = TypeHelper.GetTypeName(method.ContainingType, NameFormattingOptions.None);
+            var s = MemberHelper.GetMethodSignature(method, NameFormattingOptions.DocumentationId);
+            s = s.Substring(2);
+            s = s.TrimEnd(')');
+            s = TurnStringIntoValidIdentifier(s);
+            return s;
+        }
+
+        public static string TurnStringIntoValidIdentifier(string s)
+        {
+
+            // Do this specially just to make the resulting string a little bit more readable.
+            // REVIEW: Just let the main replacement take care of it?
+            s = s.Replace("[0:,0:]", "2DArray"); // TODO: Do this programmatically to handle arbitrary arity
+            s = s.Replace("[0:,0:,0:]", "3DArray");
+            s = s.Replace("[0:,0:,0:,0:]", "4DArray");
+            s = s.Replace("[0:,0:,0:,0:,0:]", "5DArray");
+            s = s.Replace("[]", "array");
+
+            // The definition of a Boogie identifier is from BoogiePL.atg.
+            // Just negate that to get which characters should be replaced with a dollar sign.
+
+            // letter = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz".
+            // digit = "0123456789".
+            // special = "'~#$^_.?`".
+            // nondigit = letter + special.
+            // ident =  [ '\\' ] nondigit {nondigit | digit}.
+
+            s = Regex.Replace(s, "[^A-Za-z0-9'~#$^_.?`]", "$");
+
+            s = GetRidOfSurrogateCharacters(s);
+            return s;
+        }
+
+        /// <summary>
+        /// Unicode surrogates cannot be handled by Boogie.
+        /// http://msdn.microsoft.com/en-us/library/dd374069(v=VS.85).aspx
+        /// </summary>
+        private static string GetRidOfSurrogateCharacters(string s)
+        {
+            //  TODO this is not enough! Actually Boogie cannot support UTF8
+            var cs = s.ToCharArray();
+            var okayChars = new char[cs.Length];
+            for (int i = 0, j = 0; i < cs.Length; i++)
+            {
+                if (Char.IsSurrogate(cs[i])) continue;
+                okayChars[j++] = cs[i];
+            }
+            var raw = String.Concat(okayChars);
+            return raw.Trim(new char[] { '\0' });
+        }
+        #endregion
     }
 }
