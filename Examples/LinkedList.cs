13d41e9b3f9d2bf230711f32cf01204b13867ec9
lleraromero <leandro.lera.romero@gmail.com>
2014-12-05 17:21:36 -0300

    Linear and LinkedList example

---
+++ Examples/LinkedList.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections.Generic;
+using System.Diagnostics.Contracts;
+
+namespace Examples
+{
+    public class LinkedList
+    {
+        public Queue<int> buffer { get; private set; }
+        public int size { get; private set; }
+
+        public LinkedList()
+        {
+            Contract.Ensures(buffer != null);
+
+            buffer = new Queue<int>();
+            size = 0;
+        }
+
+        [ContractInvariantMethod]
+        private void Invariant()
+        {
+            Contract.Invariant(buffer == null || size >= 0);
+        }
+
+        public void Add(int e)
+        {
+            Contract.Requires(buffer != null);
+
+            buffer.Enqueue(e);
+            size++;
+        }
+
+        public void Remove()
+        {
+            Contract.Requires(buffer != null && size > 0);
+
+            Contract.Assume(buffer.Count == size);
+            buffer.Dequeue();
+            size--;
+        }
+
+        public void Destroy()
+        {
+            Contract.Requires(buffer != null);
+
+            buffer.Clear();
+            buffer = null;
+            size = -1;
+        }
+    }
+}
