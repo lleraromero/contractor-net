26e03d2bd1e71c64d0fdbbbcfdec539d22b789db
lleraromero <leandro.lera.romero@gmail.com>
2014-12-04 23:51:09 -0300

    Sketch of a corral test class

---
+++ Tests/CorralEPAsTest.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+using Contractor.Core;
+using System.IO;
+
+namespace Tests
+{
+    [TestClass]
+    public class CorralEPAsTest
+    {
+        private static Dictionary<string, TypeAnalysisResult> epas;
+
+        [ClassInitialize()]
+        public static void GenerateEPAs(TestContext tc)
+        {
+            Configuration.Initialize();
+            Configuration.TempPath = Directory.GetParent(tc.TestDir).ToString();
+            Configuration.InlineMethodsBody = true;
+
+            epas = new Dictionary<string, TypeAnalysisResult>();
+            using (var generator = new EpaGenerator(EpaGenerator.Backend.Corral))
+            {
+                var ExamplesPath = Path.GetFullPath(Path.Combine(Environment.CurrentDirectory, @"..\..\..\Examples\obj\Debug\Decl\Examples.dll"));
+                generator.LoadAssembly(ExamplesPath);
+                epas["Examples.FiniteStack"] = generator.GenerateEpa("Examples.FiniteStack");
+            }
+        }
+
+        [TestMethod]
+        public void TestStack()
+        {
+            var epa = epas["Examples.FiniteStack"].EPA;
+
+            Assert.AreEqual(new List<IState>(epa.States).Count, 4);
+        }
+    }
+}
