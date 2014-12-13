e4bd99a71423802d62530a158f0b5af8c36e7ff9
lleraromero <leandro.lera.romero@gmail.com>
2014-12-13 20:00:29 -0300

    Cleaning up examples

---
+++ Examples/GenericStackSet.cs
@@ -0,0 +1,60 @@
+﻿using System.Diagnostics.Contracts;
+
+namespace Examples
+{
+    public class GenericStackSet<T>
+    {
+        private const int capacity = 5;
+        private int size;
+        private T[] data;
+
+        [ContractInvariantMethod]
+        private void Invariant()
+        {
+            Contract.Invariant(size >= 0);
+            Contract.Invariant(size <= capacity);
+            Contract.Invariant(data.Length == capacity);
+        }
+
+        public GenericStackSet()
+        {
+            size = 0;
+            data = new T[capacity];
+        }
+
+        [Pure]
+        public int Capacity { get { return capacity; } }
+
+        [Pure]
+        public int Size { get { return size; } }
+
+        public void Push(T item)
+        {
+            Contract.Requires(Size < Capacity);
+
+            if (!Belongs(item))
+            {
+                data[size] = item;
+                size++;
+            }
+        }
+
+        public T Pop()
+        {
+            Contract.Requires(Size > 0);
+
+            return data[--size];
+        }
+
+        [Pure]
+        public bool Belongs(T item)
+        {
+            bool result = false;
+            for (int i = 0; !result && i < size; i++)
+            {
+                result = data[i].Equals(item);
+            }
+            return result;
+        }
+    }
+}
---
+++ Examples/GenericStackSetFields.cs
@@ -0,0 +1,54 @@
+﻿using System.Diagnostics.Contracts;
+
+namespace Examples
+{
+    public class GenericStackSetFields<T>
+    {
+        public const int capacity = 5;
+        public int size;
+        public T[] data;
+
+        [ContractInvariantMethod]
+        private void Invariant()
+        {
+            Contract.Invariant(size >= 0);
+            Contract.Invariant(size <= capacity);
+            Contract.Invariant(data.Length == capacity);
+        }
+
+        public GenericStackSetFields()
+        {
+            size = 0;
+            data = new T[capacity];
+        }
+
+        public void Push(T item)
+        {
+            Contract.Requires(size < capacity);
+
+            if (!Belongs(item))
+            {
+                data[size] = item;
+                size++;
+            }
+        }
+
+        public T Pop()
+        {
+            Contract.Requires(size > 0);
+
+            return data[--size];
+        }
+
+        [Pure]
+        public bool Belongs(T item)
+        {
+            bool result = false;
+            for (int i = 0; !result && i < size; i++)
+            {
+                result = data[i].Equals(item);
+            }
+            return result;
+        }
+    }
+}
