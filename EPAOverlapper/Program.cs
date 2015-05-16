836a3f28621d812272ba5e40f7417a4f581b18fe
lleraromero <leandro.lera.romero@gmail.com>
2015-05-16 16:41:25 -0300

    Parameterized overlapper

--- EPAOverlapper/Program.cs
+++ EPAOverlapper/Program.cs
@@ -1,4 +1,5 @@
 ﻿using Contractor.Core;
+using System;
 using System.IO;
 
 namespace EPAOverlapper
@@ -7,13 +8,19 @@ namespace EPAOverlapper
     {
         static void Main(string[] args)
         {
-            using (var stream = File.OpenRead(@"C:\Users\lean\Desktop\diego.xml"))
-            using (var stream2 = File.OpenRead(@"C:\Users\lean\Desktop\ListItr-Guido.xml"))
-            {
-                var epa = new EpaXmlSerializer().Deserialize(stream, @"D:\contractor-net\Examples\obj\Debug\Decl\Examples.dll");
-                var epa2 = new EpaXmlSerializer().Deserialize(stream2, @"D:\contractor-net\Examples\obj\Debug\Decl\Examples.dll");
+            string workingDir = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+            string baseEPAFile = Path.Combine(workingDir, @"prueba.xml");
+            string topEPAFile = Path.Combine(workingDir, @"epa-invariant\epas-different-inv\ListItr-Guido.xml");
 
-                using (var streamo = File.Create(@"C:\Users\lean\Desktop\diego.png"))
+            using (var stream = File.OpenRead(baseEPAFile))
+            using (var stream2 = File.OpenRead(topEPAFile))
+            {
+                string inputAssemblyPath = Path.Combine(Environment.CurrentDirectory, @"..\..\..\Examples\obj\Debug\Decl\Examples.dll");
+                var epa = new EpaXmlSerializer().Deserialize(stream, inputAssemblyPath);
+                var epa2 = new EpaXmlSerializer().Deserialize(stream2, inputAssemblyPath);
+
+                string destinationFile = Path.Combine(workingDir, @"ListItr-Overlapped-Test3.png");
+                using (var streamo = File.Create(destinationFile))
                 {
                     // e2 has to be a subgraph of e1
                     (new EpaBinarySerializer()).SerializeOverlapped(streamo, epa, epa2);
