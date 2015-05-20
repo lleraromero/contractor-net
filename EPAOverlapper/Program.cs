e6de674154ccc87a37abbc59dfeb3e0c89c4e0bc
lleraromero <leandro.lera.romero@gmail.com>
2015-05-20 16:54:13 -0300

    Refactoring

--- EPAOverlapper/Program.cs
+++ EPAOverlapper/Program.cs
@@ -1,5 +1,6 @@
 ﻿using Contractor.Core;
 using System;
+using System.Diagnostics.Contracts;
 using System.IO;
 
 namespace EPAOverlapper
@@ -9,8 +10,8 @@ namespace EPAOverlapper
         static void Main(string[] args)
         {
             string workingDir = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
-            string baseEPAFile = Path.Combine(workingDir, @"prueba.xml");
-            string topEPAFile = Path.Combine(workingDir, @"epa-invariant\epas-different-inv\ListItr-Guido.xml");
+            string baseEPAFile = Path.Combine(workingDir, @"Base.xml");
+            string topEPAFile = Path.Combine(workingDir, @"ListItr-PerfectInv.xml");
 
             using (var stream = File.OpenRead(baseEPAFile))
             using (var stream2 = File.OpenRead(topEPAFile))
@@ -19,13 +20,17 @@ namespace EPAOverlapper
                 var epa = new EpaXmlSerializer().Deserialize(stream, inputAssemblyPath);
                 var epa2 = new EpaXmlSerializer().Deserialize(stream2, inputAssemblyPath);
 
-                string destinationFile = Path.Combine(workingDir, @"ListItr-Overlapped-Test3.png");
+                string[] baseFile = baseEPAFile.Split('\\');
+                string[] topFile = topEPAFile.Split('\\');
+                string destinationFile = Path.Combine(workingDir, string.Format(@"{0}-{1}-{2}.png", baseFile[baseFile.Length - 1],
+                                                                                                    topFile[topFile.Length - 1],
+                                                                                                    DateTime.Now.ToString("dd_MM_yyyy_HHMMss")));
                 using (var streamo = File.Create(destinationFile))
                 {
                     // e2 has to be a subgraph of e1
                     (new EpaBinarySerializer()).SerializeOverlapped(streamo, epa, epa2);
                 }
-            }            
+            }
         }
     }
 }
