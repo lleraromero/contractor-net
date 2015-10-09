c15ac9f464c411f11c7932e44a1fb2a36230c384
Leandro Lera Romero <leandro.lera.romero@gmail.com>
2015-10-09 13:42:17 -0300

    Refactoring Transition

--- Core/Transition.cs
+++ Core/Transition.cs
@@ -4,46 +4,38 @@ namespace Contractor.Core
 {
     public class Transition : IEquatable<Transition>
     {
-        public Action Action { get; protected set; }
-        public State SourceState { get; protected set; }
-        public State TargetState { get; protected set; }
-        public bool IsUnproven { get; protected set; }
+        protected Action action;
+        protected State sourceState;
+        protected State targetState;
+        protected bool isUnproven;
+
+        public Action Action { get { return action; } }
+        public State SourceState { get { return sourceState; } }
+        public State TargetState { get { return targetState; } }
+        public bool IsUnproven { get { return isUnproven; } }
 
         public Transition(Action action, State source, State target, bool isUnproven)
         {
-            this.Action = action;
-            this.SourceState = source;
-            this.TargetState = target;
-            this.IsUnproven = isUnproven;
+            this.action = action;
+            this.sourceState = source;
+            this.targetState = target;
+            this.isUnproven = isUnproven;
         }
 
         public override string ToString()
         {
-            return string.Format("{0} -- {1} --> {2}", this.SourceState, this.Action.ToString(), this.TargetState);
+            return string.Format("{0} -- {1} --> {2}", this.sourceState, this.action, this.targetState);
         }
 
         #region IEquatable members
         public bool Equals(Transition other)
         {
-            return this.Action.Name.Equals(other.Action.Name) && this.SourceState.Equals(other.SourceState) && this.TargetState.Equals(other.TargetState);
-        }
-
-        public override bool Equals(object obj)
-        {
-            // Again just optimization
-            if (ReferenceEquals(null, obj)) return false;
-            if (ReferenceEquals(this, obj)) return true;
-
-            // Actually check the type, should not throw exception from Equals override
-            if (obj.GetType() != this.GetType()) return false;
-
-            // Call the implementation from IEquatable
-            return Equals((Transition)obj);
+            return this.action.Equals(other.action) && this.sourceState.Equals(other.sourceState) && this.targetState.Equals(other.targetState);
         }
 
         public override int GetHashCode()
         {
-            return this.Action.GetHashCode() ^ this.SourceState.GetHashCode() ^ this.TargetState.GetHashCode();
+            return this.action.GetHashCode() ^ this.sourceState.GetHashCode() ^ this.targetState.GetHashCode();
         }
         #endregion
     }
