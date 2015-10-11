848df74c6003e28f7e4bf239748a49a46e81f947
Leandro Lera Romero <leandro.lera.romero@gmail.com>
2015-10-10 22:41:59 -0300

    Moving CciAction to Core

--- Core.Model/Action.cs
+++ Core.Model/Action.cs
@@ -1,6 +1,5 @@
 ﻿using Microsoft.Cci;
 using System;
-using Contractor.Utils;
 
 namespace Contractor.Core.Model
 {
@@ -26,88 +25,5 @@ namespace Contractor.Core.Model
             throw new NotSupportedException("This method should not be called");
         }
         #endregion
-    }
-
-    public class CciAction : Action
-    {
-        protected IMethodDefinition method;
-        public override string Name
-        {
-            get { return method.GetUniqueName(); }
-        }
-
-        public override IMethodDefinition Method
-        {
-            get { return method; }
-        }
-
-        public CciAction(IMethodDefinition method)
-        {
-            this.method = method;
-        }
-
-        #region IEquatable
-        public override bool Equals(Action other)
-        {
-            return base.Equals((object)other) && Equals((CciAction)other);
-        }
-
-        public bool Equals(CciAction other)
-        {
-            return method.Equals(other.method);
-        }
-
-        public override int GetHashCode()
-        {
-            return method.GetHashCode();
-        }
-        #endregion
-
-        public override string ToString()
-        {
-            return method.GetDisplayName();
-        }
-    }
-
-    public class StringAction : Action
-    {
-        protected string name;
-
-        public override string Name
-        {
-            get { return name; }
-        }
-
-        public override IMethodDefinition Method
-        {
-            get { throw new NotImplementedException(); }
-        }
-
-        public StringAction(string name)
-        {
-            this.name = name;
-        }
-
-        #region IEquatable
-        public override bool Equals(Action other)
-        {
-            return base.Equals((object)other) && Equals((StringAction)other);
-        }
-
-        public bool Equals(StringAction other)
-        {
-            return name.Equals(other.name);
-        }
-
-        public override int GetHashCode()
-        {
-            return name.GetHashCode();
-        }
-        #endregion
-
-        public override string ToString()
-        {
-            return this.name;
-        }
     }
 }
---
+++ Core/CciAction.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.Cci;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Contractor.Utils;
+using Action = Contractor.Core.Model.Action;
+
+namespace Contractor.Core
+{
+    public class CciAction : Action
+    {
+        protected IMethodDefinition method;
+        public override string Name
+        {
+            get { return method.GetUniqueName(); }
+        }
+
+        public override IMethodDefinition Method
+        {
+            get { return method; }
+        }
+
+        public CciAction(IMethodDefinition method)
+        {
+            this.method = method;
+        }
+
+        #region IEquatable
+        public override bool Equals(Action other)
+        {
+            return base.Equals((object)other) && Equals((CciAction)other);
+        }
+
+        public bool Equals(CciAction other)
+        {
+            return method.Equals(other.method);
+        }
+
+        public override int GetHashCode()
+        {
+            return method.GetHashCode();
+        }
+        #endregion
+
+        public override string ToString()
+        {
+            return method.GetDisplayName();
+        }
+    }
+}
