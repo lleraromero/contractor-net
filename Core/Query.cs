d972dc4609d929809d8b7c944595147ebe04d1f8
Leandro Lera Romero <leandro.lera.romero@gmail.com>
2015-10-17 18:24:30 -0300

    Queries as a class

---
+++ Core/Query.cs
@@ -0,0 +1,30 @@
+﻿using Contractor.Core.Model;
+
+namespace Contractor.Core
+{
+    class Query
+    {
+        protected Action action;
+        public Action Action { get { return action; } }
+
+        public Query(Action action)
+        {
+            this.action = action;
+        }
+    }
+
+    class ReachableQuery : Query
+    {
+        public ReachableQuery(Action action) : base(action) { }
+    }
+
+    class UnreachableQuery : Query
+    {
+        public UnreachableQuery(Action action) : base(action) { }
+    }
+
+    class MayBeReachableQuery : Query
+    {
+        public MayBeReachableQuery(Action action) : base(action) { }
+    }
+}
