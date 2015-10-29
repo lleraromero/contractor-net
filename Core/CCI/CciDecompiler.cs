a5d975793f102ec40069c9fb0348566245fba8dd
Leandro Lera Romero <leandro.lera.romero@gmail.com>
2015-10-29 18:54:02 -0300

    Adding a CciDecompiler

---
+++ Core/CCI/CciDecompiler.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.Cci.MutableContracts;
+using System.Diagnostics.Contracts;
+using System.IO;
+
+namespace Contractor.Core
+{
+    public class CciDecompiler
+    {
+        protected CodeContractAwareHostEnvironment host;
+
+        public CciDecompiler()
+        {
+            this.host = new CodeContractAwareHostEnvironment();
+        }
+
+        public AssemblyXXX Decompile(string inputFileName, string contractsFileName)
+        {
+            Contract.Requires(!string.IsNullOrEmpty(inputFileName) && File.Exists(inputFileName));
+
+            return new CciAssembly(inputFileName, contractsFileName, this.host);
+        }
+    }
+}
