36ed83bc95d998ef19b9a1efa77919d1ffff0fd6
Leandro Lera Romero <leandro.lera.romero@gmail.com>
2015-10-31 19:00:38 -0300

    CorralAnalyzer no longer depends on CCI

--- Core/CCI/CorralAnalyzer.cs
+++ Core/CCI/CorralAnalyzer.cs
@@ -1,10 +1,8 @@
 ﻿using Contractor.Core.Model;
-using Microsoft.Cci;
 using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
-using System.Text.RegularExpressions;
 using System.Threading;
 using Action = Contractor.Core.Model.Action;
 
@@ -84,7 +82,7 @@ namespace Contractor.Core
 
             foreach (var query in queries)
             {
-                var queryName = CreateUniqueMethodName(query.Action.Method);
+                var queryName = BctTranslator.CreateUniqueMethodName(query.Action.Method);
 
                 var corralRunner = new CorralRunner(this.token);
                 var corralArgs = string.Format("{0} /main:{1} {2}", queryAssemblyPath.Replace("dll", "bpl"), queryName, Configuration.CorralArguments);
@@ -186,61 +184,5 @@ namespace Contractor.Core
 
             return new TransitionAnalysisResult(transitions);
         }
-
-        #region BCT-TranslationHelper.cs
-        protected static string CreateUniqueMethodName(IMethodReference method)
-        {
-            var containingTypeName = TypeHelper.GetTypeName(method.ContainingType, NameFormattingOptions.None);
-            var s = MemberHelper.GetMethodSignature(method, NameFormattingOptions.DocumentationId);
-            s = s.Substring(2);
-            s = s.TrimEnd(')');
-            s = TurnStringIntoValidIdentifier(s);
-            return s;
-        }
-
-        protected static string TurnStringIntoValidIdentifier(string s)
-        {
-
-            // Do this specially just to make the resulting string a little bit more readable.
-            // REVIEW: Just let the main replacement take care of it?
-            s = s.Replace("[0:,0:]", "2DArray"); // TODO: Do this programmatically to handle arbitrary arity
-            s = s.Replace("[0:,0:,0:]", "3DArray");
-            s = s.Replace("[0:,0:,0:,0:]", "4DArray");
-            s = s.Replace("[0:,0:,0:,0:,0:]", "5DArray");
-            s = s.Replace("[]", "array");
-
-            // The definition of a Boogie identifier is from BoogiePL.atg.
-            // Just negate that to get which characters should be replaced with a dollar sign.
-
-            // letter = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz".
-            // digit = "0123456789".
-            // special = "'~#$^_.?`".
-            // nondigit = letter + special.
-            // ident =  [ '\\' ] nondigit {nondigit | digit}.
-
-            s = Regex.Replace(s, "[^A-Za-z0-9'~#$^_.?`]", "$");
-
-            s = GetRidOfSurrogateCharacters(s);
-            return s;
-        }
-
-        /// <summary>
-        /// Unicode surrogates cannot be handled by Boogie.
-        /// http://msdn.microsoft.com/en-us/library/dd374069(v=VS.85).aspx
-        /// </summary>
-        protected static string GetRidOfSurrogateCharacters(string s)
-        {
-            //  TODO this is not enough! Actually Boogie cannot support UTF8
-            var cs = s.ToCharArray();
-            var okayChars = new char[cs.Length];
-            for (int i = 0, j = 0; i < cs.Length; i++)
-            {
-                if (Char.IsSurrogate(cs[i])) continue;
-                okayChars[j++] = cs[i];
-            }
-            var raw = String.Concat(okayChars);
-            return raw.Trim(new char[] { '\0' });
-        }
-        #endregion
     }
 }
