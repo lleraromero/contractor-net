35fc21787dd6455ff6dabaa92e2484ce37fd2539
Leandro Lera Romero <leandro.lera.romero@gmail.com>
2015-10-31 19:32:25 -0300

    Refactoring

--- Core/CCI/CciAssembly.cs
+++ Core/CCI/CciAssembly.cs
@@ -13,51 +13,6 @@ using Action = Contractor.Core.Model.Action;
 
 namespace Contractor.Core
 {
-    [ContractClass(typeof(IAssemblyXXXContracts))]
-    public interface IAssemblyXXX
-    {
-        ISet<string> Types();
-        ISet<Action> Constructors(string type);
-        ISet<Action> Actions(string type);
-        IMethodContract GetContractFor(IMethodDefinition method);
-    }
-
-    #region IAssemblyXXX Contracts
-    [ContractClassFor(typeof(IAssemblyXXX))]
-    abstract class IAssemblyXXXContracts : IAssemblyXXX
-    {
-        [Pure]
-        public ISet<string> Types()
-        {
-            throw new NotImplementedException();
-        }
-
-        [Pure]
-        public ISet<Action> Constructors(string typeName)
-        {
-            Contract.Requires(!string.IsNullOrEmpty(typeName));
-            Contract.Requires(Types().Contains(typeName));
-            throw new NotImplementedException();
-        }
-
-        [Pure]
-        public ISet<Action> Actions(string typeName)
-        {
-            Contract.Requires(!string.IsNullOrEmpty(typeName));
-            Contract.Requires(Types().Contains(typeName));
-            throw new NotImplementedException();
-        }
-
-        [Pure]
-        public IMethodContract GetContractFor(IMethodDefinition method)
-        {
-            Contract.Requires(method != null);
-            Contract.Ensures(Contract.Result<IMethodContract>() != null);
-            throw new NotImplementedException();
-        }
-    }
-    #endregion
-
     public class CciAssembly : IAssemblyXXX
     {
         protected IContractAwareHost host;
---
+++ Core/IAssemblyXXX.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.Cci;
+using Microsoft.Cci.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics.Contracts;
+using Action = Contractor.Core.Model.Action;
+
+namespace Contractor.Core
+{
+    [ContractClass(typeof(IAssemblyXXXContracts))]
+    public interface IAssemblyXXX
+    {
+        ISet<string> Types();
+        ISet<Action> Constructors(string type);
+        ISet<Action> Actions(string type);
+        IMethodContract GetContractFor(IMethodDefinition method);
+    }
+
+    #region IAssemblyXXX Contracts
+    [ContractClassFor(typeof(IAssemblyXXX))]
+    abstract class IAssemblyXXXContracts : IAssemblyXXX
+    {
+        [Pure]
+        public ISet<string> Types()
+        {
+            throw new NotImplementedException();
+        }
+
+        [Pure]
+        public ISet<Action> Constructors(string typeName)
+        {
+            Contract.Requires(!string.IsNullOrEmpty(typeName));
+            Contract.Requires(Types().Contains(typeName));
+            throw new NotImplementedException();
+        }
+
+        [Pure]
+        public ISet<Action> Actions(string typeName)
+        {
+            Contract.Requires(!string.IsNullOrEmpty(typeName));
+            Contract.Requires(Types().Contains(typeName));
+            throw new NotImplementedException();
+        }
+
+        [Pure]
+        public IMethodContract GetContractFor(IMethodDefinition method)
+        {
+            Contract.Requires(method != null);
+            Contract.Ensures(Contract.Result<IMethodContract>() != null);
+            throw new NotImplementedException();
+        }
+    }
+    #endregion
+}
