94c73bf400c5cf1b57423c9974ae69f24e876611
Leandro Lera Romero <leandro.lera.romero@gmail.com>
2016-01-18 20:10:37 -0300

    Disabling old tests

--- Analyzer.CodeContracts.Tests/CodeContractsEPAsTest.cs
+++ Analyzer.CodeContracts.Tests/CodeContractsEPAsTest.cs
@@ -12,54 +12,54 @@ namespace Analyzer.CodeContracts.Tests
     [TestClass]
     public class CodeContractsEPAsTest
     {
-        private const string InputFilePath = @"..\..\..\Examples\obj\Debug\Decl\Examples.dll";
-        protected static IAssemblyXXX inputAssembly;
+        //private const string InputFilePath = @"..\..\..\Examples\obj\Debug\Decl\Examples.dll";
+        //protected static IAssemblyXXX inputAssembly;
 
-        [ClassInitialize]
-        public static void GenerateEPAs(TestContext tc)
-        {
-            var ExamplesPath = Path.GetFullPath(Path.Combine(Environment.CurrentDirectory, InputFilePath));
+        //[ClassInitialize]
+        //public static void GenerateEPAs(TestContext tc)
+        //{
+        //    var ExamplesPath = Path.GetFullPath(Path.Combine(Environment.CurrentDirectory, InputFilePath));
 
-            inputAssembly = new CciDecompiler().Decompile(ExamplesPath, null);
-        }
+        //    inputAssembly = new CciDecompiler().Decompile(ExamplesPath, null);
+        //}
 
-        [TestMethod]
-        public void TestVendingMachine()
-        {
-            //var Type = FindTypeDefinitionInAssemblyWithName("VendingMachine");
-            //var epa = GenerateEpa(type)
-            //assert
-            //assert
-            GenerateEpa("Examples.VendingMachine");
-        }
+        //[TestMethod]
+        //public void TestVendingMachine()
+        //{
+        //    //var Type = FindTypeDefinitionInAssemblyWithName("VendingMachine");
+        //    //var epa = GenerateEpa(type)
+        //    //assert
+        //    //assert
+        //    GenerateEpa("Examples.VendingMachine");
+        //}
 
-        [TestMethod]
-        public void TestLinear()
-        {
-            GenerateEpa("Examples.Linear");
-        }
+        //[TestMethod]
+        //public void TestLinear()
+        //{
+        //    GenerateEpa("Examples.Linear");
+        //}
 
-        [TestMethod]
-        public void TestDoor()
-        {
-            GenerateEpa("Examples.Door");
-        }
+        //[TestMethod]
+        //public void TestDoor()
+        //{
+        //    GenerateEpa("Examples.Door");
+        //}
 
-        [TestMethod]
-        public void TestFiniteStack()
-        {
-            GenerateEpa("Examples.FiniteStack");
-        }
+        //[TestMethod]
+        //public void TestFiniteStack()
+        //{
+        //    GenerateEpa("Examples.FiniteStack");
+        //}
 
-        protected void GenerateEpa(string typeToAnalyze)
-        {
-            var epaGenerator = new EpaGenerator(new AnalyzerMock());
-            var typeDefinition = inputAssembly.Types().First(t => t.Name.Equals(typeToAnalyze));
-            var epa = epaGenerator.GenerateEpa(typeDefinition, A.Dummy<IEpaBuilder>());
-            epa.Wait();
+        //protected void GenerateEpa(string typeToAnalyze)
+        //{
+        //    var epaGenerator = new EpaGenerator(new AnalyzerMock());
+        //    var typeDefinition = inputAssembly.Types().First(t => t.Name.Equals(typeToAnalyze));
+        //    var epa = epaGenerator.GenerateEpa(typeDefinition, A.Dummy<IEpaBuilder>());
+        //    epa.Wait();
 
-            Assert.IsFalse(epa.IsFaulted);
-            Assert.IsFalse(epa.IsCanceled);
-        }
+        //    Assert.IsFalse(epa.IsFaulted);
+        //    Assert.IsFalse(epa.IsCanceled);
+        //}
     }
 }
