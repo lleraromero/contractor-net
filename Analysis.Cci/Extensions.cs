4e3b75e1e9807558c4f7cba69be90d657a4d531d
Leandro Lera Romero <leandro.lera.romero@gmail.com>
2016-01-18 21:20:49 -0300

    Making Action's method name easier to read

--- Analysis.Cci/Extensions.cs
+++ Analysis.Cci/Extensions.cs
@@ -8,37 +8,16 @@ namespace Analysis.Cci
 {
     public static class Extensions
     {
-        [Pure]
-        public static string GetUniqueName(this INamedTypeDefinition type)
-        {
-            var name = new StringBuilder();
-
-            if (type is INamespaceTypeDefinition)
-            {
-                var rootType = (INamespaceTypeDefinition) type;
-                name.Append(rootType.ContainingNamespace);
-            }
-            else if (type is INestedTypeDefinition)
-            {
-                var nestedType = (INestedTypeDefinition) type;
-                var containingType = nestedType.ContainingTypeDefinition as INamedTypeDefinition;
-                name.Append(containingType.GetUniqueName());
-            }
-
-            name.Append('.');
-            name.Append(type.Name.Value);
-
-            if (type.IsGeneric)
-            {
-                name.Append(type.GenericParameterCount);
-            }
-
-            return name.ToString();
-        }
-
         public static string GetUniqueName(this IMethodDefinition method)
         {
-            return MemberHelper.GetMethodSignature(method, NameFormattingOptions.DocumentationId);
+            var uniqueName = MemberHelper.GetMethodSignature(method,
+                NameFormattingOptions.DocumentationId & ~NameFormattingOptions.DocumentationIdMemberKind);
+            uniqueName = uniqueName.Replace("(", "")
+                                   .Replace(")", "")
+                                   .Replace(".", "_")
+                                   .Replace(",", "")
+                                   .Replace("#", "");
+            return uniqueName;
         }
 
         public static IEnumerable<INamespaceTypeDefinition> GetAnalyzableTypes(this IModule module)
