fe49bd59d6ac63f196860d67b848dfaa56510e4c
fmartinelli <fmartinelli89@gmail.com>
2016-09-13 14:08:20 -0300

    updating BCT dependencies with CCI-github (master branch with the virtual changes)

--- Analyzer.Corral/BctRunner.cs
+++ Analyzer.Corral/BctRunner.cs
@@ -21,8 +21,8 @@ namespace Analyzer.Corral
             {
                 bct.StartInfo = new ProcessStartInfo
                 {
-                    //FileName = @"..\..\..\Dependencies\BCT\BytecodeTranslator.exe",
-                    FileName = @"C:\Users\Administrador\Documents\Visual Studio 2013\Projects\BCT\bytecodetranslator\Binaries\BytecodeTranslator.exe",
+                    FileName = @"..\..\..\Dependencies\BCT\BytecodeTranslator.exe",
+                    //FileName = @"C:\Users\Administrador\Documents\Visual Studio 2013\Projects\BCT\bytecodetranslator\Binaries\BytecodeTranslator.exe",
                     Arguments = string.Join(" ", args),
                     WorkingDirectory = tmpDir,
                     CreateNoWindow = true,
