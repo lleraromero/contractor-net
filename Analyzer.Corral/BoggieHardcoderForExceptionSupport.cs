ed5a7a6fd5fbf918e314265b020b05550922591f
fmartinelli <fmartinelli89@gmail.com>
2016-09-30 17:40:29 -0300

    Adding BoggieExceptionHardcoder to suport Exceptions/Catch for the new EPA-O queries. It should be refactorized latter to include new exceptions on demand.

---
+++ Analyzer.Corral/BoggieHardcoderForExceptionSupport.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Analyzer.Corral
+{
+    class BoggieHardcoderForExceptionSupport
+    {
+        private string full_path_to_boogie_file;
+        public BoggieHardcoderForExceptionSupport() { }
+
+        public void hardcodeExceptionsToFile(string file){
+            this.full_path_to_boogie_file=file;
+            hardcodeExceptionEqualsToNullToQueries();
+            hardcodeSubtypeAxioms();
+        }
+
+        private void hardcodeSubtypeAxioms()
+        {
+            StringBuilder stringBuilder = new StringBuilder();
+            hardcodeExceptionNotSubtypeOf(stringBuilder);
+            harcodeAllSubtypeOfException(stringBuilder);
+            harcodeSubtypeOfThemSelves(stringBuilder);
+            writeAxiomsToFile(stringBuilder,this.full_path_to_boogie_file);
+        }
+
+        private void writeAxiomsToFile(StringBuilder stringBuilder, string p)
+        {
+
+            var input = File.ReadAllText(this.full_path_to_boogie_file);
+            string pattern = "function $Subtype(Type, Type) : bool;";
+            string replacement = pattern+System.Environment.NewLine+stringBuilder.ToString();
+            Regex rgx = new Regex(Regex.Escape(pattern));
+            string result = rgx.Replace(input, replacement);
+            File.WriteAllText(this.full_path_to_boogie_file, result);
+        }
+
+        private void harcodeSubtypeOfThemSelves(StringBuilder stringBuilder)
+        {
+            stringBuilder.AppendLine("axiom $Subtype(T$System.DivideByZeroException(), T$System.DivideByZeroException());");
+            stringBuilder.AppendLine("axiom $Subtype(T$System.Exception(), T$System.Exception());");
+            stringBuilder.AppendLine("axiom $Subtype(T$System.NullReferenceException(), T$System.NullReferenceException());");
+            stringBuilder.AppendLine("axiom $Subtype(T$System.IndexOutOfRangeException(), T$System.IndexOutOfRangeException());");
+            stringBuilder.AppendLine("axiom $Subtype(T$System.OverflowException(), T$System.OverflowException());");
+
+        }
+
+        private void harcodeAllSubtypeOfException(StringBuilder stringBuilder)
+        {
+            stringBuilder.AppendLine("axiom $Subtype(T$System.DivideByZeroException(), T$System.Exception());");
+            stringBuilder.AppendLine("axiom $Subtype(T$System.NullReferenceException(), T$System.Exception());");
+            stringBuilder.AppendLine("axiom $Subtype(T$System.IndexOutOfRangeException(), T$System.Exception());");
+            stringBuilder.AppendLine("axiom $Subtype(T$System.OverflowException(), T$System.Exception());");
+        }
+
+        private void hardcodeExceptionNotSubtypeOf(StringBuilder stringBuilder)
+        {
+            stringBuilder.AppendLine("axiom !$Subtype(T$System.Exception(), T$System.DivideByZeroException());");
+            stringBuilder.AppendLine("axiom !$Subtype(T$System.Exception(), T$System.NullReferenceException());");
+            stringBuilder.AppendLine("axiom !$Subtype(T$System.Exception(), T$System.OverflowException());");
+            stringBuilder.AppendLine("axiom !$Subtype(T$System.Exception(), T$System.IndexOutOfRangeException());");
+        }
+
+        private void hardcodeExceptionEqualsToNullToQueries()
+        {
+            string lineToAdd = "$Exception := null;";
+
+            var input = File.ReadAllText(this.full_path_to_boogie_file);
+
+            string query_pattern = @"(^implementation(.*)STATE(.*)\n\{\r\n(\s\svar(.*)\n)*)";//"(implementation(.*)STATE(.)*\n{\n(.*var.*;\n)*)";
+            string replacement = "$1\n"+lineToAdd+"\n";
+            Regex rgx = new Regex(query_pattern,RegexOptions.Multiline);
+            string result = rgx.Replace(input, replacement);
+
+            File.WriteAllText(this.full_path_to_boogie_file, result);
+        }
+    }
+}
--- Analyzer.Corral/CorralRunner.cs
+++ Analyzer.Corral/CorralRunner.cs
@@ -28,6 +28,60 @@ namespace Analyzer.Corral
 
             MyLogger.LogCorral(args);
 
+            //THIS SHOULD BE NOT HERE.  
+            var boogieHarcoder = new BoggieHardcoderForExceptionSupport();
+            boogieHarcoder.hardcodeExceptionsToFile(queryAssembly.FullName);
+            //--
+            using (var corral = new Process())
+            {
+                corral.StartInfo = new ProcessStartInfo
+                {
+                    FileName = @"..\..\..\Dependencies\Corral\corral.exe",
+                    Arguments = args,
+                    WorkingDirectory = tmpDir,
+                    CreateNoWindow = true,
+                    WindowStyle = ProcessWindowStyle.Hidden,
+                    RedirectStandardOutput = true,
+                    RedirectStandardError = true,
+                    UseShellExecute = false
+                };
+
+                Logger.Log(LogLevel.Info, "=============== CORRAL ===============");
+                corral.OutputDataReceived += (sender, e) =>
+                {
+                    output.AppendLine(e.Data);
+                    Logger.Log(LogLevel.Debug, e.Data);
+                };
+                corral.ErrorDataReceived += (sender, e) => { Logger.Log(LogLevel.Fatal, e.Data); };
+                corral.Start();
+                corral.BeginErrorReadLine();
+                corral.BeginOutputReadLine();
+                corral.WaitForExit();
+
+                if (corral.ExitCode != 0)
+                {
+                    throw new Exception("Error executing corral");
+                }
+            }
+
+            Directory.Delete(tmpDir, true);
+
+            return ParseResultKind(output.ToString(), query);
+        }
+
+        public QueryResult ExecuteAfterHarcodeExceptions(FileInfo queryAssembly, Query query)
+        {
+            var tmpDir = Path.Combine(workingDir.FullName, Guid.NewGuid().ToString());
+            Directory.CreateDirectory(tmpDir);
+
+            var args = string.Format("{0} /main:{1} {2}", queryAssembly.FullName, BctMethodNamer.CreateUniqueMethodName(query), corralArguments);
+            var output = new StringBuilder();
+
+            MyLogger.LogCorral(args);
+
+            var boogieHarcoder = new BoggieHardcoderForExceptionSupport();
+            boogieHarcoder.hardcodeExceptionsToFile(queryAssembly.FullName);
+
             using (var corral = new Process())
             {
                 corral.StartInfo = new ProcessStartInfo
