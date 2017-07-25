5d72ec44514ba9b0f4a3229728a935ca92def3f5
Unknown <fmartinelli89@gmail.com>
2017-07-25 13:47:14 -0300

    adding console tests

--- Console.Tests/Tests_Stack_mscorlib.cs
+++ Console.Tests/Tests_Stack_mscorlib.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System.Diagnostics.Contracts;
 using System.IO;
+using System.Text.RegularExpressions;
 
 namespace Console.Tests
 {
@@ -17,7 +18,7 @@ namespace Console.Tests
         public void Test_EPA_Stack_mscorlib()
         {
             string outputDir = @"C:\Users\Fernan\Documents\epa-i-o-results\Stack-mscorlib\StackMscorlib\StackMscorlib";
-            string[] args = { };
+            string[] args = @"-i C:\Users\Fernan\Documents\epa-i-o-results\Stack-mscorlib\StackMscorlib\StackMscorlib\obj\Debug\Decl\StackMscorlib.dll -g C:\Users\Fernan\Documents\epa-i-o-results\Stack-mscorlib\StackMscorlib\StackMscorlib\EPA -tmp C:\Users\Fernan\Desktop\temp --il true -t System.Collections2.Stack -b Corral --xml".Split(' ') ;
             int r = Contractor.Console.Program.Main(args);
             Contract.Assert(r == 0);
             //xml exists
@@ -26,7 +27,7 @@ namespace Console.Tests
             Contract.Assert(File.Exists(outputDir + "\\Stack.png"));
 
             int number_of_transitions = Utils.number_of_transitions(outputDir + "\\Stack.xml");
-            Contract.Assert(number_of_transitions == 28);
+            Contract.Assert(number_of_transitions == 30);
         }
 
         [TestMethod]
@@ -81,8 +82,8 @@ namespace Console.Tests
     {
         public static int number_of_transitions(string xml_file)
         {
-            //count the number of transitions
-            throw new NotImplementedException();
+            //Count the number of occurrences of the word "transition"
+            return (new Regex(@"(?i)\btransition\b")).Matches(File.ReadAllText(xml_file)).Count;
         }
     }
 }
