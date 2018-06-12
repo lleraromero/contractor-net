1166e0cff25a77cb139e8855ed7345fac2d75192
Unknown <fmartinelli89@gmail.com>
2018-06-12 18:59:42 -0300

    Adding test environment

---
+++ SourceCodeInstrumenter/ExampleToTest/StackMscorlib/Example.cs
@@ -0,0 +1,15 @@
+
+using System;
+public class Example   
+{
+    private int _size;
+    public Example()
+	{
+        _size /= 0;
+    }
+    public void m()
+    {
+        _size /= 0;
+    }
+}
+            
